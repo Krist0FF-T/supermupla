22861c65c7c21be9d72b446c9981c76b12f998c2
Krist0FF-T <155083075+Krist0FF-T@users.noreply.github.com>
2024-07-27 20:02:06 +0200

    update

--- hmm/config/players.py
+++
@@ -1,9 +0,0 @@
-import .keys
-
-player_profiles = [
-    uno,
-    duo,
-    trio,
-]
-
-
--- hmm/default_levels/__init__.py
+++
@@ -1,8 +0,0 @@
-from .tiles import tiles
-import supermupla.level_config as lc
-
-levels = lc.LevelGroup(
-    name="default",
-    start_level="start_level"
-)
-
